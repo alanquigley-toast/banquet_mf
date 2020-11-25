fed19eef185f30930c49222eb8f73152381fcada
alanquigley-toast <alan.quigley@toasttab.com>
2020-11-25 21:46:48 +0000

    move spas into folder

--- spas/parent_spa/src/App.js
+++ spas/parent_spa/src/App.js
@@ -5,10 +5,11 @@ import { mountRootParcel } from 'single-spa'
 
 export function App() {
   return (
-    <div>
+    <div className='p-8 space-y-4'>
       <h1>Parent SPA</h1>
       <Button>Parent Button</Button>
       <Parcel
+        wrapClassName='p-4 bg-gray-25 rounded'
         mountParcel={mountRootParcel}
         config={() => import('child_spa/Banquet')}
       />
